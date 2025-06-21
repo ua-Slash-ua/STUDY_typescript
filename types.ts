dd31f048f48c20cbe63f71e8537ef223ae542b51
ua-Slash-ua <so.slashua@gmail.com>
2025-06-21 12:31:20 +0300

    Типи TS

---
+++ types.ts
@@ -0,0 +1,27 @@
+// Number type
+let age:number
+age = 5
+
+// String type
+let fullName:string
+fullName = 'Slash UA'
+
+// Boolean type
+let isStudy:boolean
+isStudy = true
+
+// Any type
+let line:any
+line = 123;
+line = 'asdasd';
+line = false;
+
+// Unknown type
+let line2:unknown
+line2 = "test";
+line2 = 10;
+
+
+// Void type
+(() => console.log('123'))();
+
